4500d10c7170c80516e6e34a0d1b5c6b1db0aadf
IvanGunawan058 <ivangunawann058@gmail.com>
2022-07-08 00:24:20 +0700

    moviecatalogue

---
+++ app/src/main/java/com/ivan/aplikasimovie/MovieApiInterface.kt
@@ -0,0 +1,11 @@
+package com.ivan.aplikasimovie
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface MovieApiInterface {
+
+
+    @GET("/3/movie/popular?api_key=6f0dbdd0b93e77012c4a05ce097c8805")
+    fun getMovieList(): Call<MovieResponse>
+}
